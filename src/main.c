4cfaac717669da6fba0d8a6c3af36c29b6a36121
d-vinine <dhanvinmalhotra10@gmail.com>
2025-06-04 16:10:56 +0530

    Changed renderer to render all bodies

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "simulation_interface.h"
 #include "simulation_renderer.h"
 #include <raylib.h>
+#include <stdio.h>
 
 void init_sim(const SimulationCore *core) {
   sim_init_galaxy(core->bodies, core->params, 0, core->bodies->count, 1e3, 20,
@@ -12,8 +13,8 @@ void init_sim(const SimulationCore *core) {
 }
 
 int main(void) {
-  SimulationParams params = {.body_count = 20000, 
-                             .thread_count = 50,
+  SimulationParams params = {.body_count = 10000, 
+                             .thread_count = 1,
                              .G = 0.1,
                              .eps = 0.1,
                              .dt = 0.001,
@@ -31,6 +32,7 @@ int main(void) {
   while (!WindowShouldClose()) {
     sim_core_step(core);
     sim_render_frame(core, &cam, init_sim);
+    printf("FPS: %d\n", GetFPS()); 
   }
 
   sim_core_destroy(core);
